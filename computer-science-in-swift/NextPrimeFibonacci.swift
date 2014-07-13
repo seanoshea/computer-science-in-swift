11bf0acb1b7740acaf84cb8bcb0b005be37f8c2a
seanoshea <oshea.ie@gmail.com>
2014-07-13 17:46:10 +0100

    memoization of fibonacci function

--- computer-science-in-swift/NextPrimeFibonacci.swift
+++ computer-science-in-swift/NextPrimeFibonacci.swift
@@ -3,25 +3,27 @@ import UIKit
 
 class NextPrimeFibonacci {
     
+    var map = Dictionary<Int, Int>()
+    
     func nextPrimeFibonacci(input: Int) -> Int {
-        var nextPrimeFibonacci = 0;
+        var nextPrimeFibonacci = 0
         while (true) {
             var num = fibonacci(nextPrimeFibonacci)
             if (num > input && isPrime(num)) {
                 nextPrimeFibonacci = num
                 break;
             }
-            nextPrimeFibonacci++;
+            nextPrimeFibonacci++
         }
         return nextPrimeFibonacci
     }
     
     func isPrime(input: Int) -> Bool {
-        var isPrime = input >= 2;
+        var isPrime = input >= 2
         if (isPrime) {
             for (var i = 2; i < input - 1; i++) {
                 if (input % i == 0) {
-                    isPrime = false;
+                    isPrime = false
                     break;
                 }
             }
@@ -31,9 +33,15 @@ class NextPrimeFibonacci {
     
     func fibonacci(input: Int) -> Int {
         if (input <= 1) {
-            return 1;
+            return 1
         } else {
-            return fibonacci(input - 1) + fibonacci(input - 2)
+            let memoized = map[input]
+            if (memoized) {
+                return memoized!
+            } else {
+                map[input] = fibonacci(input - 1) + fibonacci(input - 2)
+                return map[input]!
+            }
         }
     }
 }
